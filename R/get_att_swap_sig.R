26d79457c240a235567bdd7c4c9249bf3dae6c3f
Epsian <jared.n.joseph@gmail.com>
2019-12-19 17:19:26 -0800

    add combined function

---
+++ R/get_att_swap_sig.R
@@ -0,0 +1,16 @@
+#' get_att_swap_sig
+#'
+#' Transform data and report differance between real and simulated networks.
+#'
+#' @param combined_att_swaps The result of \code{combine_net_att_sims}.
+#' @param swapped_att Character. The name of the attribute that was swapped.
+#'
+#' @return A report of differances.
+#' @export
+#'
+#' @examples
+get_att_swap_sig = function(combined_att_swaps, swapped_att){
+  long = nattswap_transform(combined_att_swaps)
+  means = nattswap_means(long)
+  nattswap_z(means, swapped_att)
+}
